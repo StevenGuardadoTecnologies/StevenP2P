a14edef84a409af46f10af36187f00b8ce801179
steven Guardado <ericksteven20042910@gmail.com>
2025-04-27 19:18:55 +0200

    initial configuration

---
+++ P2P-Lending/src/main/java/com/Steven_Tecnologies/P2P_Lending/P2PLendingApplication.java
@@ -0,0 +1,13 @@
+package com.Steven_Tecnologies.P2P_Lending;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P2PLendingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P2PLendingApplication.class, args);
+	}
+
+}
---
+++ P2P-Lending/src/test/java/com/Steven_Tecnologies/P2P_Lending/P2PLendingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Steven_Tecnologies.P2P_Lending;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P2PLendingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
